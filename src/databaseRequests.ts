ae58c69ab318211cebfaef83e58e6d40bfc5a981
Lucas Fontes de Oliveira <lucas.fontes04@gmail.com>
2021-07-08 17:44:51 -0300

    Checagem de usuário existente

--- src/databaseRequests.ts
+++ src/databaseRequests.ts
@@ -40,29 +40,30 @@ async function getUser(req: Request,res: Response){
 
 	const reqId:string = req.params.user_id;
 
+	let usersJSON:TDatabase
+
 	try {
 		const readReturn:string = await fs.readFile(databasePath,'utf8');
 		
-		const usersJSON:TDatabase = JSON.parse(readReturn);
-		
-		if(usersJSON.users.length !== 0) {
-			usersJSON.users.some((user:TUser, index:number) => {
-				if(reqId === user.id){
-					return res.json(user)
-				} else if( index === usersJSON.users.length -1) {
-					console.log("Ele entrou no else if")
-					return res.status(500).send("Usuário não encontrado");
-				}
-			})
-		} else {
-			return res.status(500).send("Não temos mais usuários");
-		}
+		usersJSON = JSON.parse(readReturn);
 
 	} catch(err) {
 		console.error(err);
 		return res.status(500).send("Erro ao ler arquivo durante seleção de usuário")
 	}
     
+	if(usersJSON.users.length !== 0) {
+		usersJSON.users.some((user:TUser, index:number) => {
+			if(reqId === user.id){
+				return res.json({ users:[user]})
+			} else if( index === usersJSON.users.length -1) {
+				return res.status(500).send("Usuário não encontrado");
+			}
+		})
+	} else {
+		return res.status(500).send("Não temos mais usuários");
+	}
+
 };
 
 // Criar único usuário // Body: JSON (email,senha) // Params: ID
@@ -71,6 +72,12 @@ async function createUser (req: Request,res: Response){
 	const newId:string = uuid();
 	const newEmail:string = req.body.email;
 	const newPassword:string = req.body.password;
+
+	const newUser = {
+		"id": newId,
+		"email": newEmail,
+		"password": newPassword
+	}
 
 	if( newEmail === "" || newPassword === "" ) {
 		return res.status(500).send("Campo vazio!")
@@ -82,14 +89,21 @@ async function createUser (req: Request,res: Response){
 		const readReturn:string = await fs.readFile(databasePath,'utf8');
 		
 		const usersJSON:TDatabase = JSON.parse(readReturn);
-		usersJSON.users.push({
-			"id": newId,
-			"email": newEmail,
-			"password": newPassword
-		})
 		
+		try{
+			usersJSON.users.some((user:TUser)=>{
+				if(user.email === newEmail) {
+					throw new Error();
+				}
+			})
+		} catch(err) {
+			return res.status(500).send("Um usuário já existe com esse email")
+		}
+
+		usersJSON.users.push(newUser)
 		usersStringified = JSON.stringify(usersJSON,null, 2);
 
+		
 	} catch(err) {
 		console.error(err);
 		return res.status(500).send("Erro ao ler arquivo durante criação de usuário")
@@ -112,13 +126,11 @@ async function updateUser(req: Request,res: Response){
 	
 	const reqId:string = req.params.user_id;
 	const reqNewEmail:string = req.body.email;
-	const reqNewPassword:string = req.body.newPassword;
-
+	const reqNewPassword:string = req.body.password;
 
 	if( reqNewEmail === "" || reqNewPassword === "" ) {
 		return res.status(500).send("Campo vazio!")
 	}
-	
 
 	let usersJSON:TDatabase;
 
@@ -133,27 +145,34 @@ async function updateUser(req: Request,res: Response){
 	}
 
 	if(usersJSON.users.length !== 0) {
-		usersJSON.users.some( async (user:TUser, index:number) => {
+		usersJSON.users.some( (user:TUser, index:number) => {
 			if( reqId === user.id ){
-	
+
 				usersJSON.users.splice(index,1)
 				usersJSON.users.push({
 					"id": reqId,
 					"email": reqNewEmail,
 					"password": reqNewPassword
 				})
-	
+				
 				let usersStringified = JSON.stringify(usersJSON,null, 2);
-					
-				try {
-					await fs.writeFile(databasePath, usersStringified,'utf8')
-	
-					return res.send("Usuário atualizado com sucesso!")
-					
-				} catch(err) {
-					console.error(err);
-					return res.status(500).send("Erro ao atualizar usuário!")
+				
+				const writeDatabase = async () => {
+
+					try {
+						await fs.writeFile(databasePath, usersStringified,'utf8')
+		
+						return res.send("Usuário atualizado com sucesso!")
+						
+					} catch(err) {
+						console.error(err);
+						return res.status(500).send("Erro ao atualizar usuário!")
+					}
+
 				}
+
+				return writeDatabase().then(res => {return res});
+			
 			} else if( index === usersJSON.users.length -1) {
 				return res.status(500).send("Usuário não encontrado");
 			}
@@ -161,7 +180,7 @@ async function updateUser(req: Request,res: Response){
 	} else {
 		return res.status(500).send("Não temos mais usuários");
 	}
-
+	
 };
 
 
@@ -205,22 +224,25 @@ async function deleteUser (req: Request,res: Response){
 
 	if(usersJSON.users.length !== 0) {
 
-		usersJSON.users.some( async (user:TUser, index:number) => {
+		usersJSON.users.some( (user:TUser, index:number) => {
 			if( reqId === user.id ){
 	
 				usersJSON.users.splice(index,1)
 				let usersStringified = JSON.stringify(usersJSON,null, 2);
 	
-
-				try {
-					await fs.writeFile(databasePath, usersStringified,'utf8')
-			
-					return res.send("Usuário Deletado com sucesso!")
-					
-				} catch(err) {
-					console.error(err);
-					return res.status(500).send("Erro ao deletar usuário!")
+				const writeDatabase = async () => {
+					try {
+						await fs.writeFile(databasePath, usersStringified,'utf8')
+				
+						return res.send("Usuário Deletado com sucesso!")
+						
+					} catch(err) {
+						console.error(err);
+						return res.status(500).send("Erro ao deletar usuário!")
+					}
 				}
+
+				return writeDatabase().then(res => {return res});
 
 			} else if( index === usersJSON.users.length -1) {
 				return res.status(500).send("Usuário não encontrado!");
